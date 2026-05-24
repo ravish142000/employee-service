80c41eb4aeef8e5c9b3ce2dc15bf2c73fd27b282
ravish142000 <ravish142000@gmail.com>
2026-05-24 15:52:55 +0530

    Initial Spring Boot application

---
+++ src/main/java/com/company/demo/EmployeeController.java
@@ -0,0 +1,18 @@
+package com.company.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class EmployeeController {
+
+    @GetMapping("/employees")
+
+    public String employees() {
+
+        return "Employee Service Running Java 21";
+
+    }
+
+}
